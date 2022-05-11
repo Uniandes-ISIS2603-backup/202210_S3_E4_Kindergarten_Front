0ae3439345ba4ed657e93d3e026145e87d5c05aa
Esteban <e.gonzaleza@uniandes.edu.co>
2022-05-10 23:48:26 -0500

    prueba lista informacion niños

---
+++ src/app/prueba_ninos/prueba_ninos.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Prueba_ninosComponent } from './prueba_ninos.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [Prueba_ninosComponent],
+  exports: [Prueba_ninosComponent]
+})
+export class Prueba_ninosModule { }
